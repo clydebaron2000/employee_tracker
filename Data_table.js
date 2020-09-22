4465671e51824e21aa7a577033c1dcfce171a9e8
Clyde Baron Rapinan <clydebaron2000@gmail.com>
2020-09-21 23:13:38 -0700

    shortened code

--- Data_table.js
+++ Data_table.js
@@ -3,63 +3,31 @@ class Data_Table {
     constructor(query, table_name) {
         this.query = query;
         this.table_name = table_name;
-        this.header = "";
-        this.rows = [];
-        this.table = "";
-        this.json_fancy = [];
-        this.json_raw = [];
-        this.table_raw = [];
-        this.keys = [];
         let query_string = "*";
         switch (this.table_name) {
             case "employee":
-                query_string = `SELECT e.id as id, 
-                e.first_name AS First_Name, 
-                e.last_name AS Last_Name, 
-                title AS Title, 
-                salary AS Salary, 
-                name AS Department,
-                CONCAT(m.first_name, " ", m.last_name) AS Manager
-                 FROM employee e
-                 LEFT JOIN employee m ON e.manager_id = m.id 
-                 INNER JOIN role r ON e.role_id = r.role_id 
-                 INNER JOIN department d ON r.department_id = d.department_id`
+                query_string = `SELECT e.id as id, e.first_name AS First_Name, e.last_name AS Last_Name, title AS Title, salary AS Salary, name AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager
+                 FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role r ON e.role_id = r.role_id INNER JOIN department d ON r.department_id = d.department_id`
                 break;
             case "role":
-                query_string = `SELECT r.role_id as id, 
-                r.title AS Title, 
-                r.salary AS Salary, 
-                d.name AS Department 
-                FROM role r
-                INNER JOIN department d ON r.department_id = d.department_id`
+                query_string = `SELECT r.role_id as id, r.title AS Title, r.salary AS Salary, d.name AS Department 
+                FROM role r INNER JOIN department d ON r.department_id = d.department_id`
                 break;
             case "department":
                 query_string = `SELECT department_id as id,name as Department_Name from department`
                 break;
         }
-        this.query_string = query_string;
+        this.fancy_query_string = query_string;
     }
     async get_content_from_server() {
         try {
-            //get fancy
-            const json_fancy = JSON.parse(JSON.stringify(await this.query(this.query_string)));
-            var { output, header, rows } = this.obj_list_to_table_string(json_fancy);
-            this.json_fancy = json_fancy;
+            const json_fancy = JSON.parse(JSON.stringify(await this.query(this.fancy_query_string)));
+            const json_raw = JSON.parse(JSON.stringify(await this.query(`SELECT * FROM ${this.table_name}`)));
+            this.store_json_to_values(json_fancy, json_raw);
+            //following values don't change unless table changes
             this.column_names = Object.keys(json_fancy[0]);
-            this.header = header;
-            this.rows = rows;
-            this.table = output;
-            //get raw
-            const table_raw = await this.query(`SELECT * FROM ${this.table_name}`);
-            const json_raw = JSON.parse(JSON.stringify(table_raw));
-            var { output, header, rows } = this.obj_list_to_table_string(json_raw);
             this.keys = Object.keys(json_raw[0]);
             this.id_name = this.keys[0];
-            this.json_raw = json_raw;
-            this.table_raw = table_raw;
-            this.table_raw_print = output;
-            this.header_raw = header;
-            this.rows_raw = rows;
             this.column_types = JSON.parse(JSON.stringify(await this.query(`SHOW FIELDS FROM ${this.table_name}`))).map(column_obj => column_obj.Type).slice(1);
             return this;
         } catch (err) {
@@ -67,11 +35,21 @@ class Data_Table {
             throw err;
         };
     }
+    store_json_to_values(json_fancy, json_raw) {
+        var { output, header, rows } = this.obj_list_to_table_string(json_fancy);
+        this.json_fancy = json_fancy;
+        this.header = header;
+        this.rows = rows;
+        this.table = output;
+        var { output, header, rows } = this.obj_list_to_table_string(json_raw);
+        this.json_raw = json_raw;
+        this.table_raw = output;
+        this.header_raw = header;
+        this.rows_raw = rows;
+    }
     verify_type(column_types_list, input_list) {
         if (column_types_list.length != input_list.length) {
-            console.log("err: verify_type(): list inputs do not have the same length");
-            console.log(`column_types_list: ${column_types_list.length}\n input_list: ${input_list.length}`);
-            throw 'err';
+            throw `err: verify_type(): list inputs do not have the same length\ncolumn_types_list: ${column_types_list.length}\n input_list: ${input_list.length}`;
         }
         let i = 0,
             bool_array = [],
@@ -99,14 +77,11 @@ class Data_Table {
     }
     obj_list_to_table_string(input_obj_list) {
         const chart_matrix = Object.keys(input_obj_list[0]).map(key => {
-            var column = [key, ...(input_obj_list.map(obj => `${obj[key]}`))];
-            column = column.map((el) => {
-                while (el.length < Math.max(...(column.map(el => el.length)))) {
-                    el += " ";
-                }
+            var column = [key, ...(input_obj_list.map(obj => `${obj[key]}`))]
+            return column.map((el) => {
+                while (el.length < Math.max(...(column.map(el => el.length)))) el += " ";
                 return el;
             });
-            return column;
         });
         let i = 0,
             header_line, list_row_lines_raw = [];
@@ -125,12 +100,6 @@ class Data_Table {
         }).join('\n');
         return { output: output, header: header_line, rows: list_row_lines_raw };
     }
-    get_table() {
-        return this.table;
-    }
-    get_table_raw() {
-        return this.table_raw;
-    }
     async remove_row_by_id(id) {
         try {
             await this.query(`delete from ${this.table_name} where ${this.id_name}=${id}`)
@@ -140,11 +109,10 @@ class Data_Table {
     async add_row(elementObj) {
         try {
             const column_keys = this.keys.slice(1);
-            const column_types = this.column_types;
-            const { bool_array } = this.verify_type(column_types, Object.keys(elementObj).map(key => elementObj[key]));
+            const { bool_array } = this.verify_type(this.column_types, Object.keys(elementObj).map(key => elementObj[key]));
             if (bool_array.includes(false)) {
                 console.log("err: add_element(): elementObj attribute do not match database types");
-                console.log(`database column types (sql): ${column_types}`);
+                console.log(`database column types (sql): ${this.column_types}`);
                 console.log(`input data types (js): ${Object.keys(elementObj).map(key => typeof (elementObj[key]))}`);
                 return await this.get_content_from_server();
             }
@@ -152,7 +120,7 @@ class Data_Table {
                 console.log('err: add_element(): columns do not match');
                 console.log(Object.keys(elementObj), column_keys);
                 console.log(typeof Object.keys(elementObj), typeof column_keys);
-                return await this.get_content_from_server();;
+                return await this.get_content_from_server();
             }
             await this.query(`INSERT INTO ${this.table_name} SET ?`, elementObj);
             return await this.get_content_from_server();
@@ -161,38 +129,17 @@ class Data_Table {
     async update_value_by_id_column(value, id, column_index) {
         try {
             const database_column = this.keys.slice(1)[column_index];
-            // console.log(`Test: updating ${this.table_name} row with ${this.id_name} ${id} column ${database_column} with (${value})\n`);
             await this.query(`UPDATE ${this.table_name} SET ${database_column} = '${value}' WHERE  ${this.id_name} = ${id};`);
-            return await this.get_content_from_server(); //update value;
         } catch (err) { console.log(err); }
     }
     async sort_by_column(column_index) {
         try {
-            // console.log(column_index, this.column_names.length);
-            const column_name = this.column_names[column_index];
-            // console.log(column_name);
-            const ordered_raw_fancy = await this.query(this.query_string + ` order by ${column_name}`)
-            const ordered_json_fancy = JSON.parse(JSON.stringify(ordered_raw_fancy));
-            var ordered_raw = [],
-                ordered_json_raw = []
+            const ordered_json_fancy = JSON.parse(JSON.stringify(await this.query(this.fancy_query_string + ` order by ${this.column_names[column_index]}`)));
+            var ordered_json_raw = [];
             ordered_json_fancy.forEach(row => {
-                const id = row.id;
-                ordered_json_raw.push(this.json_raw.find((element) => element[this.id_name] === id));
-                ordered_raw.push(this.table_raw.find((element) => element[this.id_name] === id));
+                ordered_json_raw.push(this.json_raw.find((element) => element[this.id_name] === row.id));
             });
-            // console.log(ordered_json_fancy);
-            // console.log(ordered_json_raw);
-            var { output, header, rows } = this.obj_list_to_table_string(ordered_json_raw);
-            this.rows = rows;
-            this.table_raw = ordered_raw;
-            this.json_raw = ordered_json_raw;
-            this.table_raw = output;
-            this.rows_raw = rows;
-            this.header_raw = header;
-            var { output, header, rows } = this.obj_list_to_table_string(ordered_json_fancy);
-            this.json_fancy = ordered_json_fancy; //fancy json changes with output
-            this.header = header;
-            this.table = output;
+            this.store_json_to_values(ordered_json_fancy, ordered_json_raw);
             return output;
         } catch (err) {
             throw err;
--- index.js
+++ index.js
@@ -3,13 +3,9 @@ const inquirer = require("inquirer");
 const util = require("util");
 const chalk = require("chalk");
 var Data_Table = require("./Data_table");
+const { prototype } = require("stream");
 let d, r, e;
-
-function text_field_limit(str) {
-    return str.split(" ").map(word => {
-        if (word.length > 0) return word[0].toUpperCase() + word.substr(1).toLowerCase()
-    }).join(" ");
-}
+const line = new inquirer.Separator('--------');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -17,12 +13,22 @@ var connection = mysql.createConnection({
     password: "root",
     database: "emp_db"
 });
+
+function capitalize_each_word(str) {
+    return str.split(" ").map(word => {
+        if (word.length > 0) return word[0].toUpperCase() + word.substr(1).toLowerCase()
+    }).join(" ");
+}
+
+function validate_strings(input) {
+    return (input > 30) ? `err: input too long. can't be longer than 30 chars` : true;
+}
 async function view_rearrange_menu(data_table) {
-    console.log(chalk `Table: {yellow.bold ${data_table.table_name}}\n\n${data_table.get_table()}\n\n`);
+    console.log(chalk `Table: {yellow.bold ${data_table.table_name}}\n\n${data_table.table}\n\n`);
     const { column_index } = await inquirer.prompt({
         type: "list",
         name: "column_index",
-        choices: [...data_table.column_names, new inquirer.Separator('--------'), chalk `{red.bold cancel}`, new inquirer.Separator('--------')],
+        choices: [...data_table.column_names, line, chalk `{red.bold cancel}`, line],
         message: "Choose a column to sort by:",
         filter: (choice) => data_table.column_names.indexOf(choice)
     });
@@ -31,38 +37,31 @@ async function view_rearrange_menu(data_table) {
     return view_rearrange_menu(data_table);
 }
 async function add_row(data_table) {
-    let { output } = data_table.obj_list_to_table_string(data_table.json_raw);
-    console.log(chalk `Displaying raw table {yellow.bold ${data_table.table_name}}:\n${output}\n`);
+    console.log(chalk `Displaying raw table {yellow.bold ${data_table.table_name}}:\n${data_table.obj_list_to_table_string(data_table.json_raw)}\n`);
     switch (data_table.table_name) {
         case "employee":
-            return add_to_e(data_table);
+            return add_to_employee(data_table);
         case "role":
-            return add_to_r(data_table);
+            return add_to_role(data_table);
         case "department":
-            return add_to_d(data_table);
+            return add_to_department(data_table);
         default:
             throw "err";
     }
 }
-async function add_to_e(e) {
+async function add_to_employee(e) {
     const { first_name, last_name, role_id, manager_id } = await inquirer.prompt([{
         type: "input",
         name: "first_name",
         message: chalk `What is the employee's {yellow.bold first name}?`,
-        filter: text_field_limit,
-        validate: (input) => {
-            if (input > 30) return `err: input too long. can't be longer than 30 chars`;
-            else return true;
-        }
+        filter: capitalize_each_word,
+        validate: validate_strings
     }, {
         type: "input",
         name: "last_name",
         message: chalk `What is employee's {yellow.bold last name}?`,
-        filter: text_field_limit,
-        validate: (input) => {
-            if (input > 30) return `err: input too long. can't be longer than 30 chars`;
-            else return true;
-        }
+        filter: capitalize_each_word,
+        validate: validate_strings
     }, {
         type: "list",
         name: "role_id",
@@ -78,16 +77,13 @@ async function add_to_e(e) {
     }, ]);
     return e.add_row({ first_name: first_name, last_name, last_name, role_id: role_id, manager_id: manager_id });
 }
-async function add_to_r(r) {
+async function add_to_role(r) {
     const { title, salary, d_id } = await inquirer.prompt([{
         type: "input",
         name: "title",
         message: chalk `What is the {yellow.bold title} of this role?`,
-        filter: text_field_limit,
-        validate: (input) => {
-            if (input > 30) return `err: input too long. can't be longer than 30 chars`;
-            else return true;
-        }
+        filter: capitalize_each_word,
+        validate: validate_strings
     }, {
         type: "input",
         name: "salary",
@@ -108,44 +104,36 @@ async function add_to_r(r) {
     }, ]);
     return r.add_row({ title: title, salary: salary, department_id: d_id });
 }
-async function add_to_d(d) {
+async function add_to_department(d) {
     const { input } = await inquirer.prompt({
         type: "input",
         name: "input",
         message: "What is the Department_name?",
-        filter: text_field_limit,
-        validate: (input) => {
-            if (input > 30) return `err: input too long. can't be longer than 30 chars`;
-            else return true;
-        }
-    });;
+        filter: capitalize_each_word,
+        validate: validate_strings
+    });
     return d.add_row({ name: input });
 }
 async function delete_row(data_table) {
     const { index } = await inquirer.prompt({
         type: "list",
         name: "index",
-        choices: [...data_table.rows, new inquirer.Separator('--------'), chalk `{red.bold cancel}`, new inquirer.Separator('--------')],
+        choices: [...data_table.rows, line, chalk `{red.bold cancel}`, line],
         message: `Which row would you like to delete?\n  ${data_table.header}\n`,
         filter: (choice) => data_table.rows.indexOf(choice)
     });
-    if (index === -1) return data_table; //canceling
-    let id = data_table.json_raw[index][data_table.id_name];
-    // console.log(id);
-    await data_table.remove_row_by_id(id);
-    return data_table;
+    return await data_table.remove_row_by_id(data_table.json_raw[index][data_table.id_name]);
 }
 async function modify_value(data_table) {
     try {
         const { row_index } = await inquirer.prompt({
             type: "list",
             name: "row_index",
-            choices: [...data_table.rows_raw, new inquirer.Separator('--------'), chalk `{red.bold cancel}`, new inquirer.Separator('--------')],
+            choices: [...data_table.rows_raw, line, chalk `{red.bold cancel}`, line],
             message: chalk `Which {yellow.bold row} would you like to modify?\n  ${data_table.header_raw} `,
             filter: (choice) => data_table.rows_raw.indexOf(choice)
         });
-        if (row_index === -1) return data_table; //canceling
-        const id = data_table.json_raw[row_index][data_table.id_name]; //json_raw is reorganized with rows
+        if (row_index === -1) return data_table;
         const { col_index } = await inquirer.prompt({
             type: "list",
             name: "col_index",
@@ -153,16 +141,14 @@ async function modify_value(data_table) {
             message: chalk `Which {yellow.bold column} would you like to modify?\n  ${data_table.header_raw}\n  ${data_table.rows_raw[row_index]}`,
             filter: (choice) => data_table.keys.slice(1).indexOf(choice)
         });
-        if (col_index === -1) return data_table;
         let value_q;
-        console.log(data_table.column_types[col_index]);
         switch (data_table.column_types[col_index].slice(0, data_table.column_types[col_index].indexOf('('))) {
             case "varchar":
                 value_q = {
                     type: "input",
                     name: "value",
                     message: chalk `What is the {yellow.bold string value} you want for this location?`,
-                    filter: text_field_limit,
+                    filter: capitalize_each_word,
                     validate: (input) => {
                         if (input > 30) return `err: input too long. can't be longer than 30 chars`;
                         else return true
@@ -222,17 +208,14 @@ async function total_salary_by_department(data_table) {
         const { department } = await inquirer.prompt({
             type: "list",
             name: "department",
-            choices: [...valid_departments, new inquirer.Separator('--------'), chalk `{red.bold cancel}`, new inquirer.Separator('--------')],
-            message: ""
+            choices: [...valid_departments, line, chalk `{red.bold cancel}`, line],
+            message: chalk `What {yellow.bold department} would you like to {green.bold total}?`
         });
-        if (!valid_departments.includes(department)) return '';
-        let sum = 0;
-        data_table.json_fancy.forEach(row => {
-            if (row.Department === department) {
-                sum += parseFloat(row.Salary);
-            }
-        });
-        return chalk `Total of salary of {yellow.bold ${department}} is {green.bold $${sum}}\n`;
+        return chalk `Total of salary of {yellow.bold ${department}} is {green.bold $${
+            data_table.json_fancy.reduce((sum, row) => {
+            if (row.Department === department) return sum + parseFloat(row.Salary);
+                else return sum;
+        }, 0)}}\n`;
     } catch (err) {
         throw err;
     }
@@ -241,8 +224,8 @@ async function sub_menu(data_table) {
     try {
         var choices = ["view/rearrange", "add a row", "delete a row", "modify a value"];
         if (data_table.table_name === "employee") choices.push("total salary by department")
-        choices.push(new inquirer.Separator('--------'), chalk `{red.bold Return to main menu}`);
-        console.log(chalk `Table: {yellow.bold ${data_table.table_name}}\n\n${data_table.get_table()}\n\n`);
+        choices.push(line, chalk `{red.bold Return to main menu}`);
+        console.log(chalk `Table Menu: {yellow.bold ${data_table.table_name}}\n\n${data_table.table}\n\n`);
         const { choice } = await inquirer.prompt({
             type: "list",
             name: "choice",
@@ -251,63 +234,59 @@ async function sub_menu(data_table) {
         });
         switch (choice) {
             case "view/rearrange":
-                //sth
                 data_table = await view_rearrange_menu(data_table);
                 return await sub_menu(data_table);
             case "add a row":
-                //sth
                 data_table = await add_row(data_table);
                 return await sub_menu(data_table);
             case "delete a row":
-                //sth
                 data_table = await delete_row(data_table);
                 return await sub_menu(data_table);
             case "modify a value":
-                //sth
                 data_table = await modify_value(data_table);
                 return await sub_menu(data_table);
             case "total salary by department":
-                //sth
                 console.log(await total_salary_by_department(data_table));
                 return await sub_menu(data_table);
             default:
-                console.log("Returning to main menu");
                 return data_table;
         }
     } catch (err) { throw err; }
 }
-async function init() {
+async function load_values() {
     try {
         d = await (d).get_content_from_server();
         r = await (r).get_content_from_server();
         e = await (e).get_content_from_server();
-    } catch (error) {
-        throw err
+    } catch (err) {
+        throw err;
     }
 }
+const welcome = ` _                       ___                 
+|_ ._ _ ._ | _    _  _    | ._  _. _ |  _ ._ 
+|_ | | ||_)|(_)\\/(/_(/_   | |  (_|(_ |<(/_|  
+        |      /\n`;
 async function main_block() {
     try {
-        await init();
-        console.log(chalk `Table: {yellow.bold ${e.table_name}}\n\n${e.get_table()}\n\n`);
+        await load_values();
+        console.log(chalk `{yellow.bold ${welcome}}\nMain Menu\n------`)
+        console.log(chalk `Table: {yellow.bold ${e.table_name}}\n\n${e.table}\n\n`);
         const { choice } = await inquirer.prompt({
             type: "list",
             name: "choice",
-            choices: ["employees", "roles", "departments", new inquirer.Separator('--------'), chalk `{red.bold exit}`],
+            choices: ["employees", "roles", "departments", line, chalk `{red.bold exit}`],
             message: chalk `What {yellow.bold table} would you like to access?`
         });
         switch (choice) {
             case "employees":
                 e = await sub_menu(e);
-                await main_block();
-                break;
+                return await main_block();
             case "roles":
                 r = await sub_menu(r);
-                await main_block();
-                break;
+                return await main_block();
             case "departments":
                 d = await sub_menu(d);
-                await main_block();
-                break;
+                return await main_block();
             default:
                 console.log("exiting");
                 return;
@@ -317,12 +296,10 @@ async function main_block() {
 connection.connect(async function(err) {
     if (err) throw err;
     try {
-        console.log("connected as id " + connection.threadId + "\n");
         const query = util.promisify(connection.query).bind(connection);
         d = new Data_Table(query, 'department');
         r = new Data_Table(query, 'role');
         e = new Data_Table(query, 'employee');
-        await init();
         await main_block();
     } catch (err) {
         throw err;
